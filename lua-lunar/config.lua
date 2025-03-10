edb56e72312e5e786d2da9409a7e7e488e544a11
Rakesh Shrestha <rak3sh.shrestha@gmail.com>
2025-03-10 12:16:43 +0545

    latest config

--- lua-lunar/config.lua
+++ lua-lunar/config.lua
@@ -1,27 +1,35 @@
 reload('user.options');
 
-lvim.format_on_save = true
+lvim.format_on_save             = true
 lvim.builtin.cmp.cmdline.enable = true
 
 
-
--- local capabilities = require("lvim.lsp").common_capabilities()
--- capabilities.offsetEncoding = { "utf-16" }
-
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { exe = "clang-format", filetypes = { "cpp", "c" } },
-  { exe = "prettier" }
+  { exe = "clang-format", filetypes = { "cpp", "c" },
+    args = function()
+      local has_local_clang_format = vim.fn.filereadable(".clang-format") == 1
+      if has_local_clang_format then
+        return { "--style=file" }
+      else
+        return {
+          "--style={PointerAlignment: Left, BasedOnStyle: Google, IndentWidth: 6}",
+        }
+      end
+    end,
+
+  },
+  { exe = "prettier" },
 }
 
 local linters = require "lvim.lsp.null-ls.linters";
 linters.setup {
-  { name = 'eslint_d' }
+  { name = 'eslint' }
 }
 
 local codeActions = require "lvim.lsp.null-ls.code_actions"
 codeActions.setup {
-  { name = 'eslint_d' }
+  { name = 'eslint', }
 }
 
 vim.opt.wrap = true
@@ -33,68 +41,146 @@ lvim.builtin.which_key.mappings["r"] = {
   name = "Run & build",
   c    = { "<cmd>:w <CR> <cmd>term clear; g++ --std=c++17 %; if [ -f a.out ]; then time ./a.out; rm a.out; fi <CR> <cmd>startinsert<CR>", "Build C++ code" },
   d    = { "<cmd>:w <CR> <cmd>term clear; g++ --std=c++17 --debug %;<CR>", "Compile C++ with debug flags" },
+  p    = { "<cmd>:w <CR> <cmd>term clear; python3 %;<CR>", "Run with python" }
 }
 
 -- Fold
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+
+--  Folds indicator
+-- vim.opt.statuscolumn =
+--     [[%{(foldlevel(v:lnum) && foldlevel(v:lnum) > foldlevel(v:lnum - 1)) ? (foldclosed(v:lnum) == -1 ? '⌄' : '›') : ' '}]] -- Folds
+--     .. ' ' -- Spacer
+--     .. '%=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum) : " "}' -- Line number
+--     .. '%s' -- Sign
+--     .. '%{&signcolumn == "no" ? " " : ""}' -- Extra space if sign column is not enabled
+
+-- Customize tsserver settings
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+require('lspconfig').tsserver.setup({
+  on_attach = function(client, bufnr)
+    -- Your custom on_attach logic (if any)
+  end,
+  capabilities = capabilities,
+  cmd = { "typescript-language-server", "--stdio" },
+  settings = {
+    completions = {
+      completeFunctionCalls = false, -- Disable automatic parentheses
+    },
+  },
+  flags = {
+    debounce_text_changes = 150,
+  }
+})
+
+require 'lspconfig'.tailwindcss.setup({
+  settings = {
+    tailwindCSS = {
+      experimental = {
+        classRegex = {
+          { "cva\\(((?:[^()]|\\([^()]*\\))*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+          { "cx\\(((?:[^()]|\\([^()]*\\))*)\\)",  "(?:'|\"|`)([^']*)(?:'|\"|`)" }
+        },
+      },
+    },
+  },
+})
+
+--[[ require("lspconfig").tailwindcss.setup({
+  settings = {
+    tailwindCSS = {
+      classAttributes = { "class", "className", '.*ClassName', "ngClass", ".*Class" }, -- Add your custom attributes here
+    },
+  },
+})
+]]
+-- Enable fzf for telescope
+require('telescope').load_extension('fzf')
+
+
 lvim.plugins = {
   -- lazy.nvim
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      require("noice").setup({
-        cmdline = {
-          format = {
-            search_up = {
-              view = "cmdline_popup"
-            },
-            search_down = {
-              view = "cmdline_popup"
-            }
-          }
-        },
-        routes = {
-          -- Hide written messages
-          {
-            filter = {
-              event = "msg_show",
-              kind = "",
-              find = "written",
-            },
-            opts = { skip = true },
-          },
-        },
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = false,        -- use a classic bottom cmdline for search
-          command_palette = true,       -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false,       -- add a border to hover docs and signature help
-        },
-      })
-    end,
-    keys = {
-      -- Dismiss noice
-      { "<leader>nd", "<cmd>NoiceDismiss<CR>", { silent = true, desc = "Dismiss noice messages" } },
-
-    }
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  }, {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
   },
+  config = function()
+    require('notify').setup({
+      timeout = 3000,
+      stages = 'static'
+    })
+
+    require("noice").setup({
+      cmdline = {
+        format = {
+          search_up = {
+            view = "cmdline_popup"
+          },
+          search_down = {
+            view = "cmdline_popup"
+          }
+        }
+      },
+      messages = {
+        enabled = true,
+        view = "mini",
+        view_error = 'notify',
+      },
+      routes = {
+        -- Hide written messages
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "yanked",
+          },
+          opts = { skip = true },
+        },
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false,        -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,       -- add a border to hover docs and signature help
+      },
+    })
+  end,
+  keys = {
+    -- Dismiss noice
+    { "<leader>nd", "<cmd>NoiceDismiss<CR>", { silent = true, desc = "Dismiss noice messages" } },
+
+  }
+},
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
@@ -146,6 +232,7 @@ lvim.plugins = {
   },
   {
     'windwp/nvim-ts-autotag',
+    -- filetypes = "javascript, javascriptreact, typescript, typescriptreact",
     config = function()
       require('nvim-ts-autotag').setup({
         opts = { enable_close = true, enable_rename = true, enable_close_on_slash = false }
